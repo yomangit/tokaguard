2b14def3ade2b298c91bba6904e459db1ecbe217
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-05 15:00:49 +0800

    sas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-        RiskMatrixCellSeeder::class,
-        UserSeeder::class,
-        // HazardWorkflowSeeder::class
+        // RiskMatrixCellSeeder::class,
+        // UserSeeder::class,
+        HazardWorkflowSeeder::class
     ]);
     }
 }
