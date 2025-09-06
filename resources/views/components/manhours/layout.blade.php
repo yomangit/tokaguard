bcf1501375553d97b5b40d7ce8a3787a9c501629
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-06 21:49:28 +0800

    sas

--- resources/views/components/manhours/layout.blade.php
+++ resources/views/components/manhours/layout.blade.php
@@ -7,7 +7,7 @@
              h-[calc(100vh-16rem)] 
             sm:max-h-[calc(100vh-9rem)] 
             md:max-h-[calc(100vh-14rem)] 
-            lg:max-h-[calc(100vh-16rem)] 
+            lg:max-h-[calc(100vh-14rem)] 
             2xl:max-h-[41rem]">
             <div class="h-full flex-1 overflow-y-auto overflow-x-hidden rounded-xl border border-neutral-200 dark:border-base-200 p-4">
                 <div class="w-full max-w-full ">
