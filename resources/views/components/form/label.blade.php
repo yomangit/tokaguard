eec78f5cbc5e0645a90e91348cda6746251e0ab6
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-06 15:27:08 +0800

    dds

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,10 +1,6 @@
-<div>
-    <label {{ $attributes->merge(['class' => 'text-xs']) }}>
-        {{ $label }}
-        @if ($required)
-        <span class="text-red-500 font-bold">*</span>
-        @endif
-    </label>
-
-
-</div>
+<label {{ $attributes->merge(['class' => 'text-xs']) }}>
+    {{ $label }}
+    @if ($required)
+    <span class="text-red-500 font-bold">*</span>
+    @endif
+</label>
