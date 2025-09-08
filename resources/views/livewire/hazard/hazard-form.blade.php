5a72663dd1ec22122258ba0594b18bdf8da909ed
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-08 10:19:56 +0800

    ds

--- resources/views/livewire/hazard/hazard-form.blade.php
+++ resources/views/livewire/hazard/hazard-form.blade.php
@@ -65,7 +65,7 @@
                             <li class="p-2">
                                 <div class="relative w-full">
                                     <input type="text" wire:model.live="manualPelaporName" placeholder="Masukkan nama pelapor..." class="input input-bordered w-full pr-20 focus:ring-1 focus:border-info focus:ring-info focus:outline-hidden input-xs" />
-                                    <div class="!absolute top-1/2 -translate-y-1/2 right-2">
+                                    <div class="!absolute top-1/2 -translate-y-1/2 right-0">
                                         <flux:button size="xs" wire:click="addPelaporManual" icon="plus" variant="primary">
                                             Tambah
                                         </flux:button>
