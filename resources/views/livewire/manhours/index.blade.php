aeb5252f3db409095620f0d43998f16772c41fd1
yomangit <212838158+yoms-den@users.noreply.github.com>
2025-09-12 12:13:33 +0800

    sds

--- resources/views/livewire/manhours/index.blade.php
+++ resources/views/livewire/manhours/index.blade.php
@@ -30,7 +30,7 @@
                     @foreach ($data_manhours as $no => $manhour)
                     <tr>
                         <th>{{ $data_manhours->firstItem() + $no }}</th>
-                        <td>{{ \Carbon\Carbon::parse($manhour->date)->translatedFormat('F Y') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($manhour->date)->translatedFormat('mm-Y') }}</td>
                         <td>{{ $manhour->company_category }}</td>
                         <td>{{ $manhour->company }}</td>
                         <td>{{ $manhour->department }}</td>
